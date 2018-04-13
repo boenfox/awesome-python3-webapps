6e95b0248bcd7b0f586ce5e5c6bef88186170a39
zhoujing <zhoujing@oae.com.cn>
2018-04-13 12:25:37 +0800

    修正SQL脚本

--- backup/schema.sql
+++ backup/schema.sql
@@ -7,15 +7,15 @@ use awesome;
 grant select, insert, update, delete on awesome.* to 'root'@'localhost' identified by '68582188';
 
 create table users(
-    `id` varchar(50) not null
+    `id` varchar(50) not null,
     `email` varchar(50) not null,
     `passwd` varchar(50) not null,
     `admin` bool not null,
-    'name' varchar(50) not null,
+    `name` varchar(50) not null,
     `image` varchar(500) not null,
-    'create_at' real not null,
+    `create_at` real not null,
     unique key `idx_emal`(`email`),
-    key 'idx_create_at' (`create_at`),
+    key `idx_create_at` (`create_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
 
@@ -24,22 +24,22 @@ create table blogs (
     `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
-    `name` varchar not null,
+    `name` varchar(50) not null,
     `summary` varchar(200) not null,
     `content` mediumtext not null,
     `create_at` real not null,
     key `idx_create_at` (`create_at`),
     primary key (`id`)
-) engine=innode default charset=utf8;
+) engine=innodb default charset=utf8;
 
 create table comments (
     `id` varchar(50) not null,
     `blog_id` varchar(50) not null,
-    'user_id' varchar(50) not null,
+    `user_id` varchar(50) not null,
     `user_name` varchar(50) not null,
     `user_image` varchar(500) not null,
     `content` mediumtext not null,
     `create_at` real not null,
     key `idx_create_at` (`create_at`),
-    primary key ('id')
-)
+    primary key (`id`)
+) engine=innodb default charset=utf8
